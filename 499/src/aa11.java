b0f46b0b02e96985d0f291121cb0836c1cb09f2c
swang0307 <43559403+swang0307@users.noreply.github.com>
2020-09-14 22:18:06 -0700

    main

---
+++ 499/src/aa11.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class aa11 {
+
+	public static void main(String args[]) {
+		
+		
+		try {
+		Scanner abc= new Scanner(System.in);
+		System.out.print("Enter a positive integer: ");
+		int x = abc.nextInt();
+		
+		
+		
+		
+		//system allows users entering inputs and giving the results for the positive integer.
+		
+		   
+		
+		
+		
+		abc.close();
+		}catch(Exception e){
+			System.out.print("Invalid input");
+		}
+	}
+}
